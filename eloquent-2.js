c321975ac202cca0a4dd97b05ad376828aee25f6
sam.mentink <samari.b.dj@gmail.com>
2016-06-15 12:37:28 +1200

    sprint-3 5.2 upload includes answers

---
+++ eloquent-2.js
@@ -0,0 +1,42 @@
+//*looping a triangle *//
+
+var triangle = '#'{
+    for (var i = #, i <= 7; i(+=#)) {
+        '#' + # = [i];
+        console.log '#';
+    };
+}
+
+//* for (var line = "#"; line.length < 8; line += "#")
+//*    console.log(line);
+
+//* answer
+//* for (var line = "#"; line.length < 8; line += "#")
+//*  console.log(line); //*
+
+//* fizzbuzz //*
+
+//* question 2
+
+ for (var i = 1; i <= 100; i=i++) {
+
+    if (i % 3,5 == 0) {
+        console.log ("fizzbuzz");
+        };
+        else (if i % 3 == 0) {
+            console.log ("fizz");
+        };
+        else (i % 5 == 0) {
+            console.log ("buzz");
+        };
+    };
+
+//* answer...
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
+}
